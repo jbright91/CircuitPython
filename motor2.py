8fd0e20eacd0e93d1fcd32c42316e82b45f97e48
jbright91 <jbright91@charlottesvilleschools.org>
2022-09-23 09:55:39 -0400

    duplicated code for btn2

--- motor2.py
+++ motor2.py
@@ -4,19 +4,18 @@ from digitalio import DigitalInOut, Direction, Pull
 from adafruit_motor import servo
 import pwmio
 
-pwm = pwmio.PWMOut(board.D2, duty_cycle=2 ** 15, frequency=50)
 btn = DigitalInOut(board.D4)
 btn.direction = Direction.INPUT
 btn.pull = Pull.UP
+
+pwm = pwmio.PWMOut(board.D2, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 angle = 180
 
-pwm = pwmio.PWMOut(board.D2, duty_cycle=2 ** 15, frequency=50)
-btn = DigitalInOut(board.D4)
-btn.direction = Direction.INPUT
-btn.pull = Pull.UP
-my_servo = servo.Servo(pwm)
-angle = 180
+
+btn2 = DigitalInOut(board.D6)
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.UP
 
 while True:
     if not btn.value:
@@ -26,6 +25,14 @@ while True:
             time.sleep(0.1)
     else:
         print("BTN is up")
+        pass
+    if not btn2.value:
+        print("BTN2 is down")
+        for angle in range(0,0, 5):  # 0 - 180 degrees, 5 degrees at a time
+            my_servo.angle = angle 
+            time.sleep(0.1)
+    else:
+        print("BTN2 is up")
         pass
 
     time.sleep(0.1) # sleep for debounce
